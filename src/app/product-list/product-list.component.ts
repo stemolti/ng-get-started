77316279bc3b94db1b5cbf78f91300ee05a433f5
Stefano Agbodan <mystefano95@gmail.com>
2024-03-25 12:16:38 +0100

    Added component into app template

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { products } from '../products'
 
 @Component({
   selector: 'app-product-list',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+/*
+SPREAD ITERATOR
+
+Spread syntax can be used when all elements from an object or array need to be included in a new array or object, or should be applied one-by-one in a function call's arguments list. There are three distinct places that accept the spread syntax:
+
+Function arguments list (myFunction(a, ...iterableObj, b))
+Array literals ([1, ...iterableObj, '4', 'five', 6])
+Object literals ({ ...obj, key: 'value' })
+
+*/ 
+
+products = [...products];
 
 }
