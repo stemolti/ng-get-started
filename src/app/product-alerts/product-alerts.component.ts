f0b9d4f7ddc282220bcbd7d2eebc7e1bb8cad8fd
Stefano Agbodan <mystefano95@gmail.com>
2024-03-26 15:11:49 +0100

    Property binding

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -10,7 +10,11 @@ export class ProductAlertsComponent {
 
 
   /* 
+  PASS DATA TO A CHILD
+
   The @Input() decorator indicates that the property value passes in from the component's parent, ProductListComponent.
+
+  So the child component receive the input from outside to inside.
   */
 @Input()
 product: Product | undefined;
