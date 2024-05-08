6be05fa44842e2cbb6bfe3b2e26f7a51a3368c28
Stefano Agbodan <mystefano95@gmail.com>
2024-05-08 13:08:00 +0200

    Defined Checkout form model

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from '../../services/cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -7,12 +8,31 @@ import { CartService } from '../../services/cart.service';
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
-
+  constructor(
+    private cartService: CartService, private formBuilder: FormBuilder
+  ) { }
+  
   // Why items is working even before the injection of cartService?
   // Is it normal?
   items = this.cartService.getItems();
 
-  constructor(
-    private cartService: CartService
-  ) { }
+  /*
+  Inject the FormBuilder service in the CartComponent constructor(). This service is part of the ReactiveFormsModule module, which you've already imported.
+  */
+  checkoutForm = this.formBuilder.group({
+    name:'',
+    address:''
+  });
+
+  /*
+  an onSubmit() method to process the form. This method allows users to submit their name and address. In addition, this method uses the clearCart() method of the CartService to reset the form and clear the cart.
+  */
+  onSubmit(){
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+
+
+  }
+
 }
